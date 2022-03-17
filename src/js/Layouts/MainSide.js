6194b5c09d766f4a4abb88fc92ea65e27c89dea1
Clément <lowriider23@gmail.com>
2022-03-17 16:41:54 +0100

    :sparkles: create react app + remove useless generated files

---
+++ src/js/Layouts/MainSide.js
@@ -0,0 +1,15 @@
+
+const MainSide = () => {
+
+    return (
+        <div className="side-container">
+            <div className="sport-icons-container">
+                <div className="sport-icons"></div>
+                <div className="sport-icons"></div>
+                <div className="sport-icons"></div>
+                <div className="sport-icons"></div>
+            </div>
+        </div>
+    )
+}
+export default MainSide
