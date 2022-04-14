c6134ca4050400dac55d433bffeefd1802fc73f7
Clément <lowriider23@gmail.com>
2022-04-14 18:23:39 +0200

    :recycle: refacto getData request

--- src/js/Data/GetUser.js
+++ src/js/Data/GetUser.js
@@ -4,33 +4,42 @@ import Home from "../Pages/Home";
 import React from "react";
 
 const GetUser = () => {
-    const [userInfos, setUserInfos] = useState(null)
+    const [userData, setUserData] = useState(null)
 
-        /* This is a React Hook that is used to run a function when a component is mounted or updated. */
-        useEffect(() => {
-            /**
-             * It fetches data from the server and sets the userInfos state
-             */
-            const fetchData = async () => {
-                try {
-                    const res = await axios(
-                        'http://localhost:3000/user/12',
-                    )
-                    setUserInfos(res.data)
-                } catch (err) {
-                    console.log(err)
-                }
-            }
-            fetchData();
-        }, [])
+    /**
+     * It takes an array of responses from axios and returns an array of data from each response
+     * @param responses - an array of responses from the API
+     * @returns An array of data from the responses.
+     */
+    const setDataMap = (responses) => {
+        return responses.map(response => {
+            return response.data.data
+        } )
+    }
+    /* A hook that is called when the component is mounted. */
+    useEffect(() => {
+        const requestUser = axios.get("http://localhost:3000/user/12");
+        const requestActivity = axios.get("http://localhost:3000/user/12/activity");
+        const requestSessions = axios.get("http://localhost:3000/user/12/average-sessions");
 
-
+        /* A promise that is waiting for all the requests to be completed. */
+        axios.all([requestUser, requestActivity, requestSessions])
+            .then(
+                axios.spread((...responses) => {
+                    setUserData(setDataMap(responses))
+                })
+            )
+            .catch(errors => {
+                console.error(errors);
+            });
+    }, [])
+    console.log(userData)
     return (
         <>
-            {
-                userInfos !== null &&
-                <Home getUser={userInfos}/>
-            }
+            {/*{*/}
+            {/*    userData !== null &&*/}
+            {/*    <Home getData={userData}/>*/}
+            {/*}*/}
         </>
     )
 
