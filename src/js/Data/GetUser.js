f90a7cdbbf73fbef748717d1672dbd33714c4043
Clément <lowriider23@gmail.com>
2022-04-28 16:35:18 +0200

    :recycle: refacto getData

--- src/js/Data/GetUser.js
+++ src/js/Data/GetUser.js
@@ -4,8 +4,8 @@ import Home from "../Pages/Home";
 import React from "react";
 
 const GetUser = () => {
-    const [userData, setUserData] = useState(null)
-
+    const [userData, setUserData] = useState({})
+    const [isLoading, setLoading] = useState(true);
     /**
      * It takes an array of responses from axios and returns an array of data from each response
      * @param responses - an array of responses from the API
@@ -14,8 +14,12 @@ const GetUser = () => {
     const setDataMap = (responses) => {
         return responses.map(response => {
             return response.data.data
-        } )
+        })
     }
+
+    const objectKeys = ['infos', 'activity', 'sessions']
+
+
     /* A hook that is called when the component is mounted. */
     useEffect(() => {
         const requestUser = axios.get("http://localhost:3000/user/12");
@@ -26,20 +30,29 @@ const GetUser = () => {
         axios.all([requestUser, requestActivity, requestSessions])
             .then(
                 axios.spread((...responses) => {
-                    setUserData(setDataMap(responses))
+
+                    if (responses) {
+                        let newUserData = userData;
+                        objectKeys.forEach((key, index) => {
+                            newUserData[key] = responses[index].data.data
+                        })
+                        setUserData(newUserData);
+                        setLoading(false)
+                    }
+                    console.log(userData)
                 })
             )
             .catch(errors => {
                 console.error(errors);
             });
     }, [])
-    console.log(userData)
+
     return (
         <>
-            {/*{*/}
-            {/*    userData !== null &&*/}
-            {/*    <Home getData={userData}/>*/}
-            {/*}*/}
+            {
+                !isLoading &&
+                <Home userData={userData}/>
+            }
         </>
     )
 
