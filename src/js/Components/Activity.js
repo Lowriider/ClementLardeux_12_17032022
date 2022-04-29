62fcbe0bf100aadf636673687b60fec345ff79ed
Clément <lowriider23@gmail.com>
2022-04-29 23:02:23 +0200

    :memo: add documentation on activity component

--- src/js/Components/Activity.js
+++ src/js/Components/Activity.js
@@ -4,6 +4,7 @@ import {
     Bar,
     Brush,
     ReferenceLine,
+    Rectangle,
     XAxis,
     YAxis,
     CartesianGrid,
@@ -14,10 +15,15 @@ import {
 
 
 const Activity = ({activities}) => {
-    console.log(activities)
+
+    /**
+     * It returns the largest number in the array of objects
+     * @returns The maximum weight of all the activities.
+     */
     const getMaxWeight = () => {
         return activities.reduce((acc, activity) => acc = acc > activity.kilogram ? acc : activity.kilogram, 0);
     }
+
     /**
      * It takes the values of the weight property of each object in the activities object and returns the minimum and
      * maximum values of the array
@@ -33,22 +39,38 @@ const Activity = ({activities}) => {
 
         return arrayMinMaxWeight
     }
+
+    /**
+     * It takes the day property from the activities object, splits it into an array, and then returns the day as an
+     * integer
+     * @returns An array of integers representing the day of the month for each activity.
+     */
+
     const formatDates = () => {
         let arrayOfDays = Object.values(activities).map(({day}) => day.split('-'))
-        return arrayOfDays = arrayOfDays.map(date => {
+        return arrayOfDays.map(date => {
             return parseInt(date[2])
         })
     }
-    const getMinMaxDays = () => {
+    /**
+     * It takes the array of dates, formats them, and then sorts them from earliest to latest
+     * @returns An array of the dates in the format of "YYYY-MM-DD"
+     */
+    const sortMinMaxDays = () => {
 
-        let arrayMinMaxDays = formatDates().slice() // copy the array for keeping original array with order
+        let arrayMinMaxDays = formatDates().slice()
             .sort(function (a, b) {
                 return new Date(a) - new Date(b);
             })
-
         return arrayMinMaxDays
-    }
-    console.log(getMinMaxDays())
+     }
+
+    /**
+     * It takes the values of the calories property from the activities object, and then pushes the minimum value of the
+     * array minus 10, and the maximum value of the array plus 50, into a new array
+     * @returns An array of two numbers. The first number is the minimum calories minus 10. The second number is the
+     * maximum calories plus 50.
+     */
     const getMinMaxCalories = () => {
         let arrayMinMaxCalories = [];
         let arrayOfCalories = Object.values(activities).map(({calories}) => calories)
@@ -59,6 +81,11 @@ const Activity = ({activities}) => {
         return arrayMinMaxCalories
     }
 
+    /**
+     * It renders a legend for the chart
+     * @param props - The props object is passed to the renderLegend function. It contains the following properties:
+     * @returns A function that returns a div with a title and two divs with a circle and a text.
+     */
     const renderLegend = (props) => {
         const {payload} = props;
 
@@ -72,15 +99,18 @@ const Activity = ({activities}) => {
                     </div>
                 </div>
             </>
-
-        )
-            ;
+        );
     }
+
+    // const CustomCursor = props => {
+    //     const { x, y, width, height, stroke } = props;
+    //     return <Rectangle fill="red" stroke="red" x={x} y={y} width={width} height={height} />;
+    // };
 
     const data = activities
 
     return (
-        <ResponsiveContainer width="100%" height="85%">
+        <ResponsiveContainer width="100%" height="95%">
             <BarChart
                 width={50}
                 height={200}
@@ -95,13 +125,13 @@ const Activity = ({activities}) => {
                 barSize={7}
             >
                 <CartesianGrid strokeDasharray="3 3" vertical={false}/>
-                <XAxis dataKey={formatDates} domain={getMinMaxDays()} tickLine={false} tickMargin={15}/>
+                <XAxis dataKey={formatDates} domain={sortMinMaxDays()} tickLine={false} tickMargin={15} />
                 <YAxis yAxisId="right" dataKey="kilogram" height={120} orientation="right" axisLine={false}
                        tickLine={false} tick={{strokeWidth: 20}} domain={getMinMaxWeight()} tickMargin={45}/>
                 <YAxis yAxisId="left" dataKey="calories" height={120} hide={true} orientation="left"/>
-                <Tooltip viewbox={{x: 0, y: 0, width: 400, height: 400}}/>
+                <Tooltip/>
                 <Legend verticalAlign="top" iconType="circle" align="right"
-                        content={renderLegend} wrapperStyle={{lineHeight: '40px'}}/>
+                        content={renderLegend} wrapperStyle={{lineHeight: '40px', paddingBottom: "64px", paddingTop: "24px"}}/>
                 <Bar dataKey="kilogram" yAxisId="right" fill="#282D30" radius={[20, 20, 0, 0]}/>
                 <Bar dataKey="calories" yAxisId="left" fill="#E60000" radius={[20, 20, 0, 0]}/>
             </BarChart>
